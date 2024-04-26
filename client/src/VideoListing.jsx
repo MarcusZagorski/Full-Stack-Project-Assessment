2739ae06e7a1683a0bbb77ec1efd372625e80d11
Marcus <marcus.ats94@gmail.com>
2024-04-26 02:06:49 +0100

    Using DB from supabase with realtime

--- client/src/VideoListing.jsx
+++ client/src/VideoListing.jsx
@@ -1,5 +1,46 @@
+import { useState, useRef, useEffect } from "react";
+
 const VideoListing = () => {
-	return <>{/* <p>Testing video listing component</p> */}</>;
+	const [videos, setVideos] = useState([]);
+	const fetchState = useRef(true);
+
+	useEffect(() => {
+		if (fetchState.current) {
+			fetchState.current = false;
+			fetch("/api/videos")
+				.then((res) => res.json())
+				.then((data) => {
+					setVideos(data);
+					fetchState.current = true;
+				})
+				.catch((error) => {
+					console.error("There was an error fetching messages", error);
+					fetchState.current = true;
+				});
+		}
+	}, []);
+
+	console.log(videos);
+
+	const videoNames = videos.map((vids, index) => {
+		return (
+			<div key={index}>
+				<p>{vids.title}</p>
+				<iframe
+					width="560"
+					height="315"
+					src={vids.src}
+					title="YouTube video player"
+					frameborder="0"
+					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+					referrerpolicy="strict-origin-when-cross-origin"
+					allowfullscreen
+				></iframe>
+			</div>
+		);
+	});
+
+	return <>{videoNames}</>;
 };
 
 export default VideoListing;
--- server/api.js
+++ server/api.js
@@ -1,11 +1,39 @@
-import { Router } from "express";
-import db from "./db.js";
+import { Router, urlencoded } from "express";
+// import db from "./db.js";
+import { createClient } from "@supabase/supabase-js";
+
 const router = Router();
+router.use(urlencoded({ extended: true }));
+
+const supabase = createClient(
+	"https://asvgjqjkepjhmbwzyxug.supabase.co",
+	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFzdmdqcWprZXBqaG1id3p5eHVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM1NTcwMzksImV4cCI6MjAyOTEzMzAzOX0.lzqeh7SMdhZaHuXSIVQArp2nDIXZAD-Gtqxca9sexjs"
+);
 
 router.get("/videos", async (_, res) => {
-	const result = await db.query("select * from videos");
-	console.log(result);
-	res.json(result.rows);
+	// const result = await db.query("select * from videos");
+	// result
+	// 	? res.json(result.rows)
+	// 	: res
+	// 			.status(500)
+	// 			.send({ success: "false", error: "Could not connect to database" });
+	const { data } = await supabase.from("videos").select("*");
+	res.send(data);
+});
+
+router.post("/videos", async (req, res) => {
+	// const addVideo = await db.query(
+	// 	`INSERT INTO videos (title, src) VALUES ('${req.body.title}', '${req.body.src}')`
+	// );
+	// res.send("Success!");
+	const { error } = await supabase.from("videos").insert({
+		title: req.body.title,
+		src: req.body.src,
+	});
+	if (error) {
+		res.send(error);
+	}
+	res.send("Success!");
 });
 
 export default router;
