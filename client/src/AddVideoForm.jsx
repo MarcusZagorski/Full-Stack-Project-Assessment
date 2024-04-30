5e0a0a8354381e0e0fedfc7cf99e09770ae106f1
MarcusZagorski <matszagorski@gmail.com>
2024-04-30 23:23:33 +0100

    refactoring code

--- client/src/AddVideoForm.jsx
+++ client/src/AddVideoForm.jsx
@@ -1,19 +1,35 @@
 import "./AddVideoForm.css";
+import {useState} from "react"
 
-const AddVideoForm = ({ addVideo, checked, setChecked, addedVideo }) => {
-	const handleClick = () => {
-		setChecked(!checked);
+const AddVideoForm = ({setLoadVideos}) => {
+	const [checked, setChecked] = useState(false);
+	const [addedVideo, setAddedVideo] = useState(null);
+
+	const addVideo = async (e) => {
+		e.preventDefault();
+		const videoForm = Object.fromEntries(new FormData(e.target));
+	
+		const response = await fetch("/api/videos", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(videoForm),
+		});
+		if (response.ok) {
+			setAddedVideo(true);
+			setLoadVideos(true);
+			setChecked(false);
+			e.target.reset();
+		} else {
+			setAddedVideo(false);
+		}
 	};
 
-	const videoTitleInputField = () => {
-		return !checked ? null : (
-			<input
-				type="text"
-				name="title"
-				placeholder="Video title"
-				className="video-title"
-			></input>
-		);
+	const videoTitle = <h1>Add video recommendations</h1>
+	
+	const handleClick = () => {
+		setChecked(!checked);
 	};
 
 	const addedVideoMessage = () => {
@@ -29,28 +45,43 @@ const AddVideoForm = ({ addVideo, checked, setChecked, addedVideo }) => {
 		}
 	};
 
+	const submitForm = () => {
+		const videoTitleInputField = !checked ? null : (
+			<input
+				type="text"
+				name="title"
+				placeholder="Video title"
+				className="video-title"
+			></input>
+		);	
+		
+		return (
+		<form onSubmit={addVideo}>
+		<div>
+			<label className="checkbox">
+				<input type="checkbox" onClick={handleClick}></input>
+			</label>
+			<span>Add custom title?</span>
+		</div>
+			{addedVideoMessage()}
+		{videoTitleInputField}
+		<input
+			type="text"
+			name="src"
+			placeholder="Video url"
+			className="video-url"
+		></input>
+		<button type="submit" className="submit_btn">
+			SUBMIT
+		</button>
+		</form>
+	)
+}
+	
 	return (
 		<div className="add_videos_container">
-			<h1>Add video recommendations</h1>
-			{addedVideoMessage()}
-			<form onSubmit={addVideo}>
-				<div>
-					<label className="checkbox">
-						<input type="checkbox" onClick={handleClick}></input>
-					</label>
-					<span>Add custom title?</span>
-				</div>
-				{videoTitleInputField()}
-				<input
-					type="text"
-					name="src"
-					placeholder="Video url"
-					className="video-url"
-				></input>
-				<button type="submit" className="submit_btn">
-					SUBMIT
-				</button>
-			</form>
+			{videoTitle}
+			{submitForm()}
 		</div>
 	);
 };
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,40 +4,12 @@ import { useState } from "react";
 
 const App = () => {
 	const [loadVideos, setLoadVideos] = useState(true);
-	const [checked, setChecked] = useState(false);
-	const [addedVideo, setAddedVideo] = useState(null);
-
-	const addVideo = async (e) => {
-		e.preventDefault();
-		const videoForm = Object.fromEntries(new FormData(e.target));
-
-		const response = await fetch("/api/videos", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify(videoForm),
-		});
-		if (response.ok) {
-			setAddedVideo(true);
-			setLoadVideos(true);
-			setChecked(false);
-			e.target.reset();
-		} else {
-			setAddedVideo(false);
-		}
-	};
 
 	return (
 		<>
 			<h1>Video Recommendations</h1>
 			<VideoListing loadVideos={loadVideos} setLoadVideos={setLoadVideos} />
-			<AddVideoForm
-				addVideo={addVideo}
-				checked={checked}
-				setChecked={setChecked}
-				addedVideo={addedVideo}
-			/>
+			<AddVideoForm setLoadVideos={setLoadVideos} />
 		</>
 	);
 };
